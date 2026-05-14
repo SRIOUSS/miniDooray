a4ab4d38c6fa11bc6957e75ef1fe67e89a8db1da
jaena9958-art <jaena9958@gmail.com>
2026-05-14 13:37:51 +0900

    초기상태

---
+++ src/main/java/entity/Tasks.java
@@ -0,0 +1,17 @@
+package entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Tasks {
+
+
+}
