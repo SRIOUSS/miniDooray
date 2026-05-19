8c89fcdda7801c15d2c20d70b611bbf0e3f426a5
SRIOUSS <changhui5796@gmail.com>
2026-05-19 17:20:10 +0900

    init

---
+++ src/main/java/com/nhnacademy/minidoorygateway/MiniDooryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDooryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDooryGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/minidoorygateway/MiniDooryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDooryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
