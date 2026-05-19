45a56b63de116416fced0a40477aad5836ea2ebc
woalshue <sonjaemin33@gmail.com>
2026-05-20 08:57:04 +0900

    refactor: TaskViewDto에 필드 추가

--- src/main/java/com/nhnacademy/minidoorayfe/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/minidoorayfe/config/SecurityConfig.java
@@ -7,11 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
-import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
-import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
 @Slf4j
 @Configuration
--- src/main/java/com/nhnacademy/minidoorayfe/dto/task/TaskViewDto.java
+++ src/main/java/com/nhnacademy/minidoorayfe/dto/task/TaskViewDto.java
@@ -1,9 +1,12 @@
 package com.nhnacademy.minidoorayfe.dto.task;
 
+import com.nhnacademy.minidoorayfe.dto.comment.CommentResponseDto;
 import com.nhnacademy.minidoorayfe.dto.project.ProjectInfoDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -13,4 +16,5 @@ public class TaskViewDto {
     private TaskResponseDto taskResponseDto;
     private TaskInfoListDto taskInfoListDto;
     private ProjectInfoDto projectInfoDto;
+    private List<CommentResponseDto> commentResponseDtoList;
 }
